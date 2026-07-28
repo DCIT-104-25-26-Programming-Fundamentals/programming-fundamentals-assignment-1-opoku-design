bb7ed30a27ae3e2c90f6b1484e3ba550de2b19f9
opoku-design <opokuamod@gmail.com>
2026-07-28 11:16:08 -0700

    add matrices

--- assignment_04_matrix_operations.py
+++ assignment_04_matrix_operations.py
@@ -96,3 +96,19 @@ def transpose_matrix(matrix):
         transpose.append(new_row)
 
     return transpose
+
+def add_matrices(matrix1, matrix2):
+    rows = len(matrix1)
+    cols = len(matrix1[0])
+
+    result = []
+
+    for i in range(rows):
+        row = []
+
+        for j in range(cols):
+            row.append(matrix1[i][j] + matrix2[i][j])
+
+        result.append(row)
+
+    return result
