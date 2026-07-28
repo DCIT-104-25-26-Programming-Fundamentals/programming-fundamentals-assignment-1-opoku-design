13f9f9255d9ae7c3c05de3f51b586f3f38d5356f
opoku-design <opokuamod@gmail.com>
2026-07-28 08:47:17 -0700

    added input

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -47,3 +47,11 @@ def is_prime(number):
             return False
 
     return True
+
+# Main program
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is NOT a prime number.")
