53f73786a6a7c97d5964bd88b8e0e8f8577295c5
opoku-design <opokuamod@gmail.com>
2026-07-28 12:05:08 -0700

    view task

--- assignment_07_todo_list.py
+++ assignment_07_todo_list.py
@@ -83,3 +83,11 @@ def add_task(tasks):
     task = input("Enter task: ")
     tasks.append(task)
     print(f'Task added: "{task}"')
+
+def view_tasks(tasks):
+    if len(tasks) == 0:
+        print("Your to-do list is empty.")
+    else:
+        print("\nYour Tasks:")
+        for i in range(len(tasks)):
+            print(f"{i + 1}. {tasks[i]}")
